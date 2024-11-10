b15d9da11ae2d2e88687f7c2aa4a426f507f8b20
V15 G3\Dino <dinofranceschi1@gmail.com>
2024-11-10 17:45:02 +0100

    Error management

--- mala-sapa-webshop-frontend/src/app/app-routing.module.ts
+++ mala-sapa-webshop-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PocetnaComponent } from './pocetna/pocetna.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', component: PocetnaComponent },
+  {path: 'test-error', component: TestErrorComponent },
+  {path: 'server-error', component: ServerErrorComponent },
+  {path: 'not-found', component: NotFoundComponent },
   {path: 'webshop', loadChildren: () => import('./webshop/webshop.module').then(m => m.WebshopModule)},
+  {path: 'test-error', component: TestErrorComponent },
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
--- mala-sapa-webshop-frontend/src/app/app.module.ts
+++ mala-sapa-webshop-frontend/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { PocetnaModule } from './pocetna/pocetna.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
   
 @NgModule({
   declarations: [
@@ -19,7 +20,9 @@ import { PocetnaModule } from './pocetna/pocetna.module';
     CoreModule,
     PocetnaModule
 ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
